254913fa62226e3d06c3af378ad6c6cabc30a76a
Parvez Mia <parvez.pz1971@gmail.com>
2022-03-17 22:59:35 +0600

    reading file (local)

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+const express = require('express')
+const fs = require('fs')
+const multer = require('multer')
+var upload = multer({ dest: './uploads/' })
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', {fileContent: ''})
+})
+
+app.post('/', (req, res) => {
+    res.render('index', {fileContent: ''})
+})
+
+app.post('/uploadFile', upload.single('cpp_file'), (req, res) => {
+    fs.readFile(req.file.path, 'utf-8', function(err,data){
+        if(err){
+          res.end(err);
+        }
+        res.render('index', {fileContent: data.toString()});
+      });
+})
+
+app.listen(5000) ;
