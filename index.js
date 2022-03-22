31603e0b354eb0ea12344c722169f71291b420da
Parvez Mia <parvez.pz1971@gmail.com>
2022-03-22 23:15:59 +0600

    file data download

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const auth = new GoogleAuth({
   scopes: scopes
 })
 
+// create and upload the file to the specific folder in google drive
 // auth is the authenticated client, fileLocalPath is the path to the file to upload,
 // filename will be the name of the file after uploading
+// returns the file id in google drive
 const createAndUploadFile = async (auth, fileLocalPath, filename) => {
   // initialize drive service
   const drive = google.drive({version: 'v3', auth})
@@ -33,12 +35,25 @@ const createAndUploadFile = async (auth, fileLocalPath, filename) => {
     mimeType: 'text/plain',
     body: fs.createReadStream(fileLocalPath)
   }
+  // creates the file in google drive
   const file = await drive.files.create({
     resource: fileMetadata,
     media: media,
     fields: 'id'
   })
-  console.log(`File ID: ${file.data.id}`)
+  return file.data.id
+}
+
+// returns the data of the file stored in google drive given the file id
+const downloadFileData = async (auth, fileId) => {
+  // initialize drive service
+  const drive = google.drive({version: 'v3', auth})
+  // get the file data from google drive
+  const file = await drive.files.get({
+    fileId: fileId,
+    alt: 'media'
+  })
+  return file.data
 }
 
 const app = express()
@@ -53,12 +68,13 @@ app.post('/', (req, res) => {
 })
 
 app.post('/uploadFile', upload.single('cpp_file'), (req, res) => {
-    fs.readFile(req.file.path, 'utf-8', function(err,data){
+    fs.readFile(req.file.path, 'utf-8', async function(err,data){
       if(err){
           res.end(err);
         }
-        res.render('index', {fileContent: data.toString()});
-        createAndUploadFile(auth, req.file.path, 'test.cpp')
+        let fileId = await createAndUploadFile(auth, req.file.path, 'test.cpp')
+        let fileData = await downloadFileData(auth, fileId)
+        res.render('index', {fileContent: fileData.toString()});
       });
 })
 
